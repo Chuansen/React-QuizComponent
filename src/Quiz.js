c2a5a96c3f3bac2f5d456ac6e8a1f739b4d3291d
Chuansen <cbro768@aucklanduni.ac.nz>
2018-07-06 10:32:49 +0900

    complete module 4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,23 +1,38 @@
 import React, {Component} from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json')
+
+
+// const isQuizEnd = () => {
+
+// }
 
 class Quiz extends Component {
     constructor(props) {
         super(props);
         this.state = {quiz_position: 1};
     }
+    //Render is just a function. Can run normal JS in it...
     render () {
-        return(
-            <div>
-                <QuizQuestion
-                    quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}
-                />
-            </div>
-        )
+        //Gets called each time rendered
+        const isQuizEnd = this.state.quiz_position -1 === quizData.quiz_questions.length;
+        if (isQuizEnd) {
+            return(
+                <QuizEnd style={{display: true ? "" : "none"}}/>
+            )
+        }
+        else {
+            return(
+                <div>
+                    <QuizQuestion
+                        quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}
+                    />
+                </div>
+            )
+        }
     }
-
 }
 
 export default Quiz
